3912ffe96d435d05db9131880475db2bcb0d237a
ahmdhqnn <ahmadhaqinn@gmail.com>
2024-11-14 22:49:48 +0700

    first commit

---
+++ src/main/java/com/boostmytool/storecrud/StorecrudApplication.java
@@ -0,0 +1,13 @@
+package com.boostmytool.storecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorecrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boostmytool/storecrud/StorecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boostmytool.storecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
