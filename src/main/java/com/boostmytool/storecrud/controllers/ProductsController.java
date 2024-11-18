e634e6021dc9a11c0d1c7e706358830d31b92f0e
ahmdhqnn <ahmadhaqinn@gmail.com>
2024-11-18 12:02:45 +0700

    create product

--- src/main/java/com/boostmytool/storecrud/controllers/ProductsController.java
+++ src/main/java/com/boostmytool/storecrud/controllers/ProductsController.java
@@ -5,12 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.boostmytool.storecrud.models.Product;
 import com.boostmytool.storecrud.models.ProductDto;
 import com.boostmytool.storecrud.services.ProductsRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/products")
@@ -32,5 +38,19 @@ public class ProductsController {
         ProductDto productDto = new ProductDto();
         model.addAttribute("productDto", productDto);   
         return "products/create";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(
+        @Valid @ModelAttribute ProductDto productDto,
+        BindingResult result
+    ) {
+        if (productDto.getImageFile().isEmpty()) {
+            result.addError(new FieldError("productDto", "imageFile", "The image file is required"));
+        }
+        if (result.hasErrors()) {
+            return "products/CreateProduct";
+        }
+            return "redirect:/products";
     }
 }
